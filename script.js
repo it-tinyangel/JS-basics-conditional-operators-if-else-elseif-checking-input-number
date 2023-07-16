e4505414dd6c1c9d78e2940b174a5ad85f8271e7
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-07-17 00:48:40 +0300

    added missing number range

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let number = prompt("Enter number:");
 
-if (number <= 2 || number == 12) {
-	alert("Winter");
-} else if
-	(number <= 5) {
-	alert("Spring");
-} else if
-	(number <= 8) {
-	alert("Summer");
-} else if
-	(number <= 11) {
-	alert("Autumn");
+if (number > 0 && number <= 12) {
+	if (number <= 2 || number == 12) {
+		alert("Winter");
+	} else if
+		(number <= 5) {
+		alert("Spring");
+	} else if
+		(number <= 8) {
+		alert("Summer");
+	} else if
+		(number <= 11) {
+		alert("Autumn");
+	}
 } else {
 	alert("Impossible. Enter number from 1 to 12");
 }
